4321436a68a673d66a0d81474ad39b193e9b24d3
Kyle <Ky@Kyles-MacBook-Pro.local>
2016-04-24 23:27:04 -0500

    Facebook login Added

--- SpinderFB/ViewController.swift
+++ SpinderFB/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        view.addSubview(loginButton)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
